ea0424f39e775e4ffa0537e682dabb3899709ad4
Tanushree_Muralidaran <tanushreem11820@gmail.com>
2026-05-29 21:29:39 +0800

    fixed file path for vercel

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './App.css';
 import LoginSignUp from './Components/LoginSignUp/LoginSignUp';
 import Home from './Components/Home/Home';
-import WeeklySpread from "./Components/WeeklySpread/weeklyspread";
-import Reflection from "./Components/Reflection/Reflection";
-import Logout from "./Components/Logout/Logout";
+import WeeklySpread from "./Components/WeeklySpread/weeklyspread.jsx";
+import Reflection from "./Components/Reflection/Reflection.jsx";
+import Logout from "./Components/Logout/Logout.jsx";
 import Preferences from "./Components/Preferences/preferences.jsx"
 import Create from "./Components/Preferences/Create.jsx"
 import Update from "./Components/Preferences/Update.jsx"
-
+/* i think it has to have .jsx at the end for vercel*/
 
 
 function App() {
