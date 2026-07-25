a3d8f4cd004111d24abe9b2a15a38ce053f1ae41
Jyothsana Narendran <jyo@Jyothsanas-MacBook-Pro.local>
2026-07-26 02:35:58 +0800

    fixing smtp gmail

--- server/services/emailService.js
+++ server/services/emailService.js
@@ -1,3 +1,18 @@
+import nodemailer from "nodemailer";
+
+
+console.log(
+  "EMAIL_USER:",
+  process.env.EMAIL_USER
+);
+
+console.log(
+  "APP PASSWORD LENGTH:",
+  process.env.EMAIL_APP_PASSWORD?.length
+);
+
+
+
 function createTransporter() {
 
   return nodemailer.createTransport({
@@ -8,23 +23,126 @@ function createTransporter() {
 
     secure: false,
 
+    family: 4,
+
+
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_APP_PASSWORD,
     },
 
 
-    tls: {
-      rejectUnauthorized: false,
-    },
+    connectionTimeout: 10000,
 
+    greetingTimeout: 10000,
 
-    connectionTimeout: 30000,
-
-    greetingTimeout: 30000,
-
-    socketTimeout: 30000,
+    socketTimeout: 10000,
 
   });
+
+}
+
+
+
+function timeoutPromise(promise, ms) {
+
+  return Promise.race([
+
+    promise,
+
+    new Promise((_, reject) => {
+
+      setTimeout(() => {
+
+        reject(
+          new Error(
+            "Email sending timed out"
+          )
+        );
+
+      }, ms);
+
+    }),
+
+  ]);
+
+}
+
+
+
+export async function sendEmail(
+  to,
+  subject,
+  text
+) {
+
+  console.log(
+    "Preparing email:",
+    to
+  );
+
+
+  const transporter = createTransporter();
+
+
+
+  try {
+
+    console.log(
+      "SMTP sending started:",
+      to
+    );
+
+
+    const info = await timeoutPromise(
+
+      transporter.sendMail({
+
+        from:
+          `"DopaMIND" <${process.env.EMAIL_USER}>`,
+
+        to,
+
+        subject,
+
+        text,
+
+      }),
+
+      20000
+
+    );
+
+
+    console.log(
+      "EMAIL SENT:",
+      info.messageId
+    );
+
+
+    await transporter.close();
+
+
+    return info;
+
+
+  } catch(error) {
+
+
+    console.error(
+      "EMAIL FAILED:",
+      error.message
+    );
+
+
+    try {
+      transporter.close();
+    } catch {}
+
+
+
+    throw error;
+
+  }
 
 }
